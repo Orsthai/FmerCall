7d4ce14a5ad96b1a21fcc7c0c3bd710caaf715b6
Orathai <orathai.li59@snru.com>
2018-02-20 12:01:19 +0700

    Add Fragment Success

--- app/src/main/java/masterkung/snru/ac/th/fmercall/MainActivity.java
+++ app/src/main/java/masterkung/snru/ac/th/fmercall/MainActivity.java
@@ -3,11 +3,24 @@ package masterkung.snru.ac.th.fmercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterkung.snru.ac.th.fmercall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//       Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+
+        }
+
+
+    }    // Main Method
+
+}   // Main Class
