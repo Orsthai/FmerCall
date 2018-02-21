5e69e5febae08c87843d550557edcd75128c8540
Orathai <orathai.li59@snru.com>
2018-02-21 14:17:16 +0700

    Lais

--- app/src/main/java/masterkung/snru/ac/th/fmercall/fragment/MainFragment.java
+++ app/src/main/java/masterkung/snru/ac/th/fmercall/fragment/MainFragment.java
@@ -1,11 +1,15 @@
 package masterkung.snru.ac.th.fmercall.fragment;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import masterkung.snru.ac.th.fmercall.R;
 
@@ -14,6 +18,48 @@ import masterkung.snru.ac.th.fmercall.R;
  */
 
 public class MainFragment extends Fragment {
+
+//    Explicit
+    private String tag = "MyTagV1";
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //        For Station 1
+        ImageView station1ImageView = getView().findViewById(R.id.imageViewstation1);
+        station1ImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d(tag, "You Click Image Station1");
+
+            }
+        });
+
+        TextView station1TextView = getView().findViewById(R.id.textViewStation1);
+        station1TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Log.d(tag, "Click Text " + getString(R.string.station1));
+            }
+        });
+
+                //        For Station 2
+
+
+                //        For Station 3
+
+
+                //        For Station 4
+
+
+    }   //Main Method
+
+    public void callStation(String numderCall) {
+
+    }
 
     @Nullable
     @Override
